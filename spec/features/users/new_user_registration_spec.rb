171e0b01778d169f6f61cebeb814b666d8b8c321
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-15 21:07:02 +0300

    Make scenario names more clearer in user specs

--- spec/features/users/new_user_registration_spec.rb
+++ spec/features/users/new_user_registration_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 # As a User
 # So that I can post pictures on Pixagram as me
 # I want to register for my account
-feature 'New user registration' do
-  scenario 'can create a new user account' do
+feature 'User signs up' do
+  scenario 'with valid credentials' do
     user = build :user
     sign_up_as user
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
-  scenario 'requires a username to successfully create an account' do
+  scenario 'without a username' do
     user = build(:user, username: nil)
     sign_up_as user
     expect(page).to have_content "can't be blank"
   end
 
-  scenario 'requires a username to be more than 3 characters' do
+  scenario 'having a username with less than 3 characters' do
     user = build(:user, username: 'w')
     sign_up_as user
     expect(page).to have_content 'minimum is 3 characters'
   end
 
-  scenario 'requires a username to be less 16 characters' do
+  scenario 'having a username with more than 16 characters' do
     user = build(:user, username: 'johndoeknucklesjames')
     sign_up_as user
     expect(page).to have_content('maximum is 16 characters')
