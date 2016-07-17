1d4a8a2922c295c7b0103ff33b173cee042070e5
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-17 21:11:59 +0300

    Add the javascript response when creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,10 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "Your comment has been added."
-      redirect_to posts_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form. Something went wrong."
       render root_path
