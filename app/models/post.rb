af4e5ffb3768e6b15cfb04ccfaf3ad95ec02e956
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-21 02:10:25 +0300

    Add length limitations to description in a Post model
    minimum: 3, maximum: 300

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
   validates :user_id, presence: true
   validates :image, presence: true
+  validates_length_of :description, in: 3..300
 
-  has_many :comments, dependent: :destroy
-  belongs_to :user
-  has_attached_file :image, :styles => { :medium => "600x" },
-                    :default_url => "missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "600x" },
+                    default_url: "missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
