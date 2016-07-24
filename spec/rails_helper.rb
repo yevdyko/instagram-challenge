654a2fe851341fb8e7db4e793151c9c164da3771
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-24 18:19:28 +0300

    Fix styling bugs in css

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'selenium-webdriver'
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
