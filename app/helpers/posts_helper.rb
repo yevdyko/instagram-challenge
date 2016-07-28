3a14991a5c2c7ed5e4a6ff5fe4ebc20f3220d339
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-28 04:17:36 +0300

    Add username to the post’s list of likers using AJAX
    As a User
    So that once I’ve liked a post
    I want to see my username added to the post’s list of likers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,21 @@
 module PostsHelper
+  def likers_of(post)
+    votes = post.votes_for.up.by_type(User)
+    usernames = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        usernames.push(link_to voter.username,
+                               'http://www.example.com',
+                               class: 'username')
+      end
+      usernames.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  private
+
+  def like_plural(votes)
+    return ' like this' if votes.count > 1
+    ' likes this'
+  end
 end
--- spec/features/likes/likes_feature_spec.rb
+++ spec/features/likes/likes_feature_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
 
 feature 'Liking posts' do
-  given(:user) { create :user }
+  given(:user)  { create :user }
   given!(:post) { create :post, user: user }
 
   background do
     log_in_as user
   end
 
-  scenario 'can like a post' do
+  # As a User
+  # So that I can add a ‘Like’ to the post
+  # I want to click a little heart icon under the image
+  scenario "can add 'Like' to the post" do
     visit root_path
     expect(page).to have_css 'a.glyphicon-heart-empty'
+  end
+
+  # As a User
+  # So that once I’ve liked a post
+  # I want to see my username added to the post’s list of likers
+  scenario "can see username added to the post's list of likers" do
+    visit root_path
+    expect(find('.likes')).to_not have_content user.username
+    click_link "like_#{post.id}"
+    expect(find('.likes')).to have_content user.username
   end
 end
