2da00de6f505f1e1ff55f991b8c243b98e7a50c8
Yev Dyko <evgeny.dyko@gmail.com>
2016-07-28 16:57:15 +0300

    Refactor comments feature spec

--- spec/features/comments/comments_feature_spec.rb
+++ spec/features/comments/comments_feature_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 feature 'Comments' do
-  given(:user)      { create :user }
-  given(:user_2)    { create :user }
-  given(:text)      { create :post, user: user }
-  given(:message)   { build :comment, user: user, post: text }
-  given(:message_2) { build :comment, user: user_2, post: text }
+  given(:user)     { create :user }
+  given(:user_two) { create :user }
+  given!(:post)    { create :post, user: user }
+  given(:message)  { build :comment, user: user, post: post }
 
   # As a User
   # So that I can let people know my thoughts
@@ -17,8 +16,10 @@ feature 'Comments' do
 
     scenario 'can leave a comment on an existing post' do
       write_comment_with message
-      user_should_see_username_of user
-      user_should_see_comment message
+      within '#comment' do
+        expect(page).to have_css '.username', text: user.username
+        expect(page).to have_css '.comment-content', text: message.thoughts
+      end
     end
   end
 
@@ -26,34 +27,46 @@ feature 'Comments' do
   # So that I can rectify what I originally comment
   # I want to delete my comments
   context 'deleting comments' do
+    given!(:message_two) { create :comment, user: user_two, post: post }
+
     background do
-      log_in_as user_2
-      write_comment_with message_2
+      log_in_as user_two
     end
 
     scenario 'can delete a comment on an existing post' do
-      user_should_see_comment message_2
-      user_should_see_username_of user_2
+      within '#comment' do
+        expect(page).to have_css '.username', text: user_two.username
+        expect(page).to have_css '.comment-content', text: message_two.thoughts
+      end
       delete_comment
-      user_should_not_see_comment message_2
+      within '.comments' do
+        expect(page).not_to have_css '.comment-content', text: message_two.thoughts
+      end
     end
 
     # As a User
     # So that I can protect comments from deleting by another user
     # I want to delete comments that only belong to me
-    scenario "can't delete a comment not belonging to them" do
-      log_out
-      log_in_as user
-      user_should_see_comment message_2
-      user_should_not_see_link 'Delete'
-    end
+    context "can't delete a comment not belonging to them" do
+      background do
+        log_out
+        log_in_as user
+      end
 
-    scenario "can't delete a comment not belonging to them via urls" do
-      log_out
-      log_in_as user
-      page.driver.submit :delete, "posts/4/comments/4", {}
-      user_should_see_alert "That comment doesn't belong to you!"
-      user_should_see_comment message_2
+      scenario 'via the user interface' do
+        within '#comment' do
+          expect(page).to have_css '.comment-content', text: message_two.thoughts
+          expect(page).not_to have_css '.delete-comment', text: 'Delete'
+        end
+      end
+
+      scenario 'via the URLs' do
+        page.driver.submit :delete, "posts/#{post.id}/comments/#{message_two.id}", {}
+        expect(page).to have_content "That comment doesn't belong to you!"
+        within '#comment' do
+          expect(page).to have_css '.comment-content', text: message_two.thoughts
+        end
+      end
     end
   end
 end
