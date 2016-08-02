f51374abb3c71ececf52254039f0fb050abc88c5
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-02 14:51:31 +0000

    Add displaying Likes in two ways
    As a User
    So that a post has more than or equal to 4 likers
    I want to see the number of likers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,28 @@
 module PostsHelper
-  def likers_of(post)
+  def liked_post(post)
+    if user_signed_in? && current_user.voted_for?(post)
+      link_to '', unlike_post_path(post), remote: true, id: "like_#{post.id}",
+        class: 'like glyphicon glyphicon-heart'
+    else
+      link_to '', like_post_path(post), remote: true, id: "like_#{post.id}",
+        class: 'like glyphicon glyphicon-heart-empty'
+    end
+  end
+  
+  def display_likes(post)
     votes = post.votes_for.up.by_type(User)
+    return list_likers(votes) if votes.size < 4
+    count_likers(votes)
+  end
+
+  private
+
+  def like_plural(votes)
+    return ' like this' if votes.count > 1
+    ' likes this'
+  end
+  
+  def list_likers(votes)
     usernames = []
     unless votes.blank?
       votes.voters.each do |voter|
@@ -12,20 +34,8 @@ module PostsHelper
     end
   end
   
-  def liked_post(post)
-    if user_signed_in? && current_user.voted_for?(post)
-      link_to '', unlike_post_path(post), remote: true, id: "like_#{post.id}",
-        class: 'like glyphicon glyphicon-heart'
-    else
-      link_to '', like_post_path(post), remote: true, id: "like_#{post.id}",
-        class: 'like glyphicon glyphicon-heart-empty'
-    end
-  end
-
-  private
-
-  def like_plural(votes)
-    return ' like this' if votes.count > 1
-    ' likes this'
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + ' likes'
   end
 end
