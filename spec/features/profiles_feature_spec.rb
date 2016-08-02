edbcded75cd8a5d3a99d7b8a9ab60f4f57ecc0b6
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-02 19:33:58 +0000

    Add showing only the specified user's posts
    As a User
    So that I can visit a profile page
    I want to see only the specified user's posts

--- spec/features/profiles_feature_spec.rb
+++ spec/features/profiles_feature_spec.rb
@@ -7,15 +7,25 @@ feature 'Profiles' do
   given!(:post_two) { create :post, user: user_two }
   
   context 'viewing user profiles' do
+    background do 
+      log_in_as user
+      visit root_path
+      first('.username').click_link user.username
+    end
     
     # As a User
     # So that I can visit a profile page
     # I want to see the username in the URL
     scenario 'visiting a profile page shows the username in the URL' do
-      log_in_as user
-      visit root_path
-      first('.username').click_link user.username
       expect(page.current_path).to eq profile_path(user.username) 
+    end
+    
+    # As a User
+    # So that I can visit a profile page
+    # I want to see only the specified user's posts
+    scenario "a profile page only shows the specified user's posts" do
+      expect(page).to have_content post.description
+      expect(page).not_to have_content post_two.description
     end
   end
 end
