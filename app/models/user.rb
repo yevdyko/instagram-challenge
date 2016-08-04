737eba017daee4d6f7ade062565f3b10e8897bd1
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-04 19:53:12 +0000

    Add changing user profile details
    As a User
    So that I can change my own profile details
    I want to edit my profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_voter
-  
+
   validates :username, presence: true, length: { minimum: 3, maximum: 16 }
 
   devise :database_authenticatable, :registerable,
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
 end
