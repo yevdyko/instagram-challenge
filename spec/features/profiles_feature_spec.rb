254646f65652e9d50b707c88f5c4d43435670d81
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-04 17:12:46 +0000

    Add showing the edit profile page
    As a User
    So that I can visit a edit profile page
    I want to see the edit profile route

--- spec/features/profiles_feature_spec.rb
+++ spec/features/profiles_feature_spec.rb
@@ -5,27 +5,39 @@ feature 'Profiles' do
   given(:user_two)  { create :user }
   given!(:post)     { create :post, user: user }
   given!(:post_two) { create :post, user: user_two }
-  
+
+  background do
+    log_in_as user
+    first('.username').click_link user.username
+  end
+
   context 'viewing user profiles' do
-    background do 
-      log_in_as user
-      visit root_path
-      first('.username').click_link user.username
-    end
-    
+
     # As a User
     # So that I can visit a profile page
     # I want to see the username in the URL
     scenario 'visiting a profile page shows the username in the URL' do
-      expect(page.current_path).to eq profile_path(user.username) 
+      expect(page.current_path).to eq profile_path(user.username)
     end
-    
+
     # As a User
     # So that I can visit a profile page
     # I want to see only the specified user's posts
     scenario "a profile page only shows the specified user's posts" do
       expect(page).to have_content post.description
       expect(page).not_to have_content post_two.description
+    end
+  end
+
+  context 'editing user profiles' do
+
+    # As a User
+    # So that I can visit a edit profile page
+    # I want to see the edit profile route
+    scenario 'visiting the Edit profile page shows the edit profile route' do
+      click_link 'Edit Profile'
+      expect(page.current_path).to eq edit_profile_path(user.username)
+      expect(page).to have_content 'Change your profile'
     end
   end
 end
