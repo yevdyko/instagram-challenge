55643357c7925c7a46b95422f7eb8eab15d5fe06
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-05 13:33:10 +0000

    Prevent user from editing other users' profile
    As a User
    So that I can only change my own profile details
    I want to prevent other users from editing my profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_user
-  
+  before_action :owned_profile, only: [:edit, :update]
+
   def show
     @posts = User.find_by(username: params[:username]).posts.order(created_at: :desc)
   end
@@ -23,6 +24,14 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def owned_profile
+    @user = User.find_by(username: params[:username])
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
   def set_user
