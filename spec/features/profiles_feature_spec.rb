58b3167beaab4095d7877ee88c19e8bfea773267
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-05 13:33:10 +0000

    Prevent user from editing other users' profile
    As a User
    So that I can only change my own profile details
    I want to prevent other users from editing my profile

--- spec/features/profiles_feature_spec.rb
+++ spec/features/profiles_feature_spec.rb
@@ -8,10 +8,12 @@ feature 'Profiles' do
 
   background do
     log_in_as user
-    first('.username').click_link user.username
   end
 
   context 'viewing user profiles' do
+    background do
+      first('.username').click_link user.username
+    end
 
     # As a User
     # So that I can visit a profile page
@@ -30,12 +32,15 @@ feature 'Profiles' do
   end
 
   context 'editing user profiles' do
+    background do
+      first('.username').click_link user.username
+      click_link 'Edit Profile'
+    end
 
     # As a User
     # So that I can visit a edit profile page
     # I want to see the edit profile route
     scenario 'visiting the Edit profile page shows the edit profile route' do
-      click_link 'Edit Profile'
       expect(page.current_path).to eq edit_profile_path(user.username)
     end
 
@@ -43,13 +48,30 @@ feature 'Profiles' do
     # So that I can change my own profile details
     # I want to edit my profile
     scenario 'can change my own profile details' do
-      click_link 'Edit Profile'
       attach_file('user_avatar', 'spec/files/images/avatar.jpg')
       fill_in 'user_bio', with: user.bio
       click_button 'Update Profile'
       expect(page.current_path).to eq profile_path(user.username)
       expect(page).to have_css "img[src*='avatar']"
       expect(page).to have_content user.bio
+      expect(page).to have_content 'Your profile has been updated.'
+    end
+
+    # As a User
+    # So that I can only change my own profile details
+    # I want to prevent other users from editing my profile
+    context "can't edit a profile that doesn't belong to you" do
+      scenario "when visiting another user's profile" do
+        visit root_path
+        find(:xpath, "//a[contains(@href,'/#{user_two.username}')]", match: :first).click
+        expect(page).to_not have_content 'Edit Profile'
+      end
+
+      scenario 'when the url path is directly visited' do
+        visit "/#{user_two.username}/edit"
+        expect(page.current_path).to eq root_path
+        expect(page).to have_content "That profile doesn't belong to you!"
+      end
     end
   end
 end
