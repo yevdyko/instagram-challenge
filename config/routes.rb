8a3c39f99a69e4e47e42ca7f137342cb3b32be6c
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-09 16:24:01 +0000

    Update layouts and application views through i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'profiles/show'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'posts#index'
 
