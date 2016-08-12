4ab4c6ad6c0d4e0bd747d6c4d413a965beba48ad
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-12 14:59:44 +0000

    Update likes files through i18n

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -3,44 +3,42 @@ require 'rails_helper'
 feature 'Liking posts' do
   given(:user)  { create :user }
   given!(:post) { create :post, user: user }
+  background { log_in_as user }
+  after { log_out }
 
   # As a User
   # So that I can add a 'Like' to the post
   # I want to click a little heart icon under the image
   scenario "can add 'Like' to the post" do
-    log_in_as user
-    expect(page).to have_css 'a.glyphicon-heart-empty'
+    expect(page).to have_empty_heart_icon
   end
 
   # As a User
   # So that once I've liked a post
   # I want to see my username added to the post's list of likers
   scenario "can see username added to the post's list of likers" do
-    log_in_as user
-    expect(find('.likes')).to_not have_content user.username
-    click_link "like_#{post.id}"
-    expect(find('.likes')).to have_content user.username
+    expect(find('.likes')).to_not have_username user
+    add_like post
+    expect(find('.likes')).to have_username user
   end
 
   # As a User
   # So that I can revoke my decision
   # I want to unlike a post by clicking the button again
   scenario 'can unlike a post' do
-    log_in_as user
-    click_link "like_#{post.id}"
-    expect(find('.likes')).to have_content user.username
-    click_link "like_#{post.id}"
-    expect(find('.likes')).to_not have_content user.username
+    add_like post
+    expect(find('.likes')).to have_username user
+    add_unlike post
+    expect(find('.likes')).to_not have_username user
   end
 
   # As a User
   # So that once I've liked a post
   # I want to see the heart icon turn solid upon clicking it
   scenario 'can see the heart turn solid red upon liking' do
-    log_in_as user
-    expect(page).to have_css('a.glyphicon-heart-empty')
-    click_link "like_#{post.id}"
-    expect(page).to have_css('a.glyphicon-heart')
+    expect(page).to have_empty_heart_icon
+    add_like post
+    expect(page).to have_solid_heart_icon
   end
 
   # As a User
@@ -49,11 +47,13 @@ feature 'Liking posts' do
   context 'when a post has more than or equal to 4 likes' do
     scenario 'can see the number of likes' do
       created_users = create_list(:user, 4)
+
       created_users.each do |user|
-        log_in_as user
-        click_link "like_#{post.id}"
         log_out
+        log_in_as user
+        add_like post
       end
+
       expect(find('.likes')).to have_content '4 likes'
     end
   end
@@ -62,10 +62,11 @@ feature 'Liking posts' do
   # So that I can redirect from the list of likes to my profile
   # I want to link my profile to my username on the list of likes
   scenario 'can redirect from the list of likes to my profile' do
-    user_two = create :user
-    log_in_as user_two
-    click_link "like_#{post.id}"
-    first('.likes').click_link user_two.username
-    expect(page.current_path).to eq profile_path(user_two.username)
+    visit root_path
+
+    add_like post
+    first('.likes').click_link user.username
+
+    expect(page).to have_current_path profile_path(user.username)
   end
 end
