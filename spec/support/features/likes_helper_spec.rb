d678b6e5559cec29ce1bd4982ae1c53cac234e09
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-12 14:59:44 +0000

    Update likes files through i18n

--- spec/support/features/likes_helper_spec.rb
+++ spec/support/features/likes_helper_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 module LikesHelpers
-  def have_heart_icon
+  def have_empty_heart_icon
     have_css('a.glyphicon-heart-empty')
+  end
+
+  def have_solid_heart_icon
+    have_css('a.glyphicon-heart')
+  end
+
+  def add_like(post)
+    click_link "like_#{post.id}"
+  end
+
+  def add_unlike(post)
+    click_link "like_#{post.id}"
   end
 end
