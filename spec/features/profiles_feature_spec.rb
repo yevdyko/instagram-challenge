41c8e6dc5e63dbb5672676c9c14853e28067c227
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-12 15:09:14 +0000

    Update profiles feature spec

--- spec/features/profiles_feature_spec.rb
+++ spec/features/profiles_feature_spec.rb
@@ -5,27 +5,26 @@ feature 'Profiles' do
   given(:user_two)  { create :user }
   given!(:post)     { create :post, user: user }
   given!(:post_two) { create :post, user: user_two }
-  
+
   context 'viewing user profiles' do
-    background do 
+    background do
       log_in_as user
-      visit root_path
       first('.username').click_link user.username
     end
-    
+
     # As a User
     # So that I can visit a profile page
     # I want to see the username in the URL
     scenario 'visiting a profile page shows the username in the URL' do
-      expect(page.current_path).to eq profile_path(user.username) 
+      expect(page).to have_current_path profile_path(user.username)
     end
-    
+
     # As a User
     # So that I can visit a profile page
     # I want to see only the specified user's posts
     scenario "a profile page only shows the specified user's posts" do
-      expect(page).to have_content post.description
-      expect(page).not_to have_content post_two.description
+      expect(page).to have_description post
+      expect(page).not_to have_description post_two
     end
   end
 end
