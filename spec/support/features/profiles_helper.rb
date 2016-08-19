865834069d6e1dec0e29d0fdf766767d7a56ff7e
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-19 03:46:45 +0300

    Fix profiles feature spec

--- spec/support/features/profiles_helper.rb
+++ spec/support/features/profiles_helper.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 module ProfilesHelpers
+  def have_bio(user)
+    have_css('.profile-bio', text: user.bio)
+  end
+
   def have_errors_messages
     have_css('.alert.alert-danger')
   end
