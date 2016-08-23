92b222fe570232cec72d0c83be12db23b8d0200e
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-23 04:21:49 +0300

    Add showing only the last 5 unread notifications in the navbar
    As a User
    So that I can see the limited amount of notifications in the navbar
    I want to show only the last 5 unread notifications

--- spec/features/notifications_feature_spec.rb
+++ spec/features/notifications_feature_spec.rb
@@ -5,10 +5,7 @@ feature 'Notifications' do
   given(:user_two) { create :user }
   given!(:post)    { create(:post, user: user) }
 
-  # As a User
-  # So that I can find out when someone comments on my post
-  # I want to display the notification count on navbar
-  scenario 'can see the number of notifications' do
+  background do
     built_messages = build_list(:comment, 10, post: post, user: user_two)
 
     log_in_as user_two
@@ -19,31 +16,32 @@ feature 'Notifications' do
 
     log_out
     log_in_as user
-
-    expect(page).to have_icon_flag
-    expect(find('.dropdown-toggle')).to have_content '10'
   end
 
   # As a User
-  # So that I can see only unread notifications
+  # So that I can find out when someone comments on my post
+  # I want to display the notification count on navbar
+  scenario 'can see the number of notifications on navbar' do
+    expect(page).to have_icon_flag
+    expect(page).to have_notification_count '10'
+  end
+
+  # As a User
+  # So that I can see only unread notifications in the navbar
   # I want to mark visited notifications as read
-  scenario 'can see only unread notifications' do
-    built_messages = build_list(:comment, 9, post: post, user: user_two)
-
-    log_in_as user_two
-
-    built_messages.each do |message|
-      write_comment_with message
-    end
-
-    log_out
-    log_in_as user
-
-    4.times do
+  scenario 'can see only unread notifications in the navbar' do
+    6.times do
       click_link "#{user_two.username} has commented on your post", match: :first
     end
 
     expect(page).to have_current_path post_path(post)
-    expect(find('.dropdown-toggle')).to have_content '5'
+    expect(page).to have_notification_count '4'
+  end
+
+  # As a User
+  # So that I can see the limited amount of notifications in the navbar
+  # I want to show only the last 5 unread notifications
+  scenario 'can see only the last 5 unread notifications in the navbar' do
+    expect(page).to have_unread_notifications(5)
   end
 end
--- spec/support/features/notifications_helper.rb
+++ spec/support/features/notifications_helper.rb
@@ -2,4 +2,12 @@ module NotificationsHelpers
   def have_icon_flag
     have_css('span.glyphicon-flag')
   end
+
+  def have_notification_count(text)
+    have_css('.dropdown-toggle', text: text)
+  end
+
+  def have_unread_notifications(count)
+    have_css('.dropdown-menu .notification', count: count)
+  end
 end
