768a6851c2da049efd4b6005d414779fba921a1a
Yev Dyko <evgeny.dyko@gmail.com>
2016-08-30 16:01:32 +0300

    Update login form styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   get 'profiles/show'
 
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations',
+                                    sessions: 'sessions' }
 
   root 'posts#index'
 
