2b3d019b76ad3a44bf03cc4350cbb304c296d4a7
Yev Dyko <evgeny.dyko@gmail.com>
2016-10-03 14:45:49 +0300

    Add avatar near username on post header

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,9 +1,9 @@
 module ProfilesHelper
   def profile_avatar_select(user)
     return image_tag user.avatar.url(:medium), id: 'image-preview',
-      class: 'img-responsive img-circle profile-image' if user.avatar.exists?
+      class: 'img-responsive img-circle' if user.avatar.exists?
     image_tag 'default-avatar.jpg', id: 'image-preview',
-      class: 'img-responsive img-circle profile-image'
+      class: 'img-responsive img-circle'
   end
 
   def current_user_is_following(current_user_id, followed_user_id)
