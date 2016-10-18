99abeac200f838672b3d730bec6df231217e7a44
Yev Dyko <evgeny.dyko@gmail.com>
2016-10-18 16:21:28 +0300

    Refactor OmniAuthCallbacks controller
    Split #facebook into several private methods to avoid long method.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,9 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
-      @user = User.find_or_create_by(auth_hash)
+      load_or_create_user
 
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      else
-        session['devise.facebook_data'] = auth_hash
-        redirect_to new_user_registration_url, notice: t('omniauth.persisted')
-      end
+      log_in_or_create_new_user
     end
 
     def failure
@@ -21,6 +15,28 @@ module Users
 
     def auth_hash
       request.env['omniauth.auth']
+    end
+
+    def load_or_create_user
+      @user = User.find_or_create_by(auth_hash)
+    end
+
+    def log_in_or_create_new_user
+      if @user.persisted?
+        log_in_with_auth
+      else
+        create_new_user_with_auth
+      end
+    end
+
+    def log_in_with_auth
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    end
+
+    def create_new_user_with_auth
+      session['devise.facebook_data'] = auth_hash
+      redirect_to new_user_registration_url, notice: t('omniauth.persisted')
     end
   end
 end
